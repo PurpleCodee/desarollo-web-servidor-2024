c61ee02173965ffae8c7c1f47323eca6b85e9033
PurpleCodee <enyaglf.2000@gmail.com>
2024-10-12 21:28:20 +0200

    12/10/2024

--- Ejercicios-formularios/ejercicio-4.php
+++ Ejercicios-formularios/ejercicio-4.php
@@ -26,24 +26,65 @@ En los select se podrá elegir entre: CELSIUS, KELVIN y FAHRENHEIT.-->
 <label for="temperatura">temperatura</label>
 <input type="number" name="temperatura" placeholder="Introduce temperatura"><br><br>
 
-<select type="text" name="tipos_temperatura">
-    <option value="CELSIUS">CELSIUS</option>
-    <option value="KELVIN">KELVIN</option>
-    <option value="FAHRENHEIT">FAHRENHEIT</option>
-</select>
+<label for="tipos_temperatura">Convertir de: </label>
+
+<select name="tipos_temperatura">
+
+    <option value="CELSIUS">Celsius</option>
+    <option value="KELVIN">kelvin</option>
+    <option value="FAHRENHEIT">fahrenheit</option>
+
+</select><br><br>
+
+<label for="convertir">Convertir a: </label>
+
+<select name="convertir">
+
+    <option value="CELSIUS">Celsius</option>
+    <option value="KELVIN">kelvin</option>
+    <option value="FAHRENHEIT">fahrenheit</option>
+
+</select><br><br>
 
 <!--Boton-->
 <input type="submit" value="Convertir temperatura">
 
+</form>
 <!--Realizo el ejercicio-->
 
 <?php
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-       
+
+        $_temperatura = $_POST["temperatura"];//guardo los valores
+        $_desde = $_POST["tipos_temperatura"];
+        $_hasta = $_POST["convertir"];
+
+        $_conversion = "de " . strtolower($_desde) . " a " . strtolower($_hasta) . ":"; //concateno los valores en minuscula
+
+        echo "<p>Temperatura: $_temperatura</p>";
+        echo "<p>Convertir de: $_desde</p>";
+        echo "<p>Convertir a: $_hasta</p>";
+
+        //Para que el resultado salga correcto y no pase al default la cadena tiene que estar PERFECTISISISISMA :)
+
+        $resultado = match ($_conversion) {
+            "de celsius a kelvin:" => $_temperatura + 273.15,
+            "de celsius a fahrenheit:" => ($_temperatura * 9/5) + 32,
+            "de kelvin a celsius:" => ($_temperatura - 273.15),
+            "de kelvin a fahrenheit:" => (($_temperatura - 273.15) * (9/5) + 32),
+            "de fahrenheit a celsius:" => (($_temperatura - 32) * (5/9)),
+            "de fahrenheit a kelvin:" => (($_temperatura - 32) * (5/9) + 273.15),
+            "de celsius a celsius:" => $_temperatura,
+            "de kelvin a kelvin:" => $_temperatura,
+            "de fahrenheit a fahrenheit:" => $_temperatura,
+            default => "ERROR A LA HORA DE CONVERTIR!!"
+        };
+        echo "<h2>Resultado: $resultado</h2>"; 
     }
+    
 ?>
 
-</form>
+
     
 </body>
 </html>
